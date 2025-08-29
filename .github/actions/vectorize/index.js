91ad7f1629c54c8a2e752c33db4d61f70eeb8d23
andrey-lawyer <uvarov.advokat@gmail.com>
2025-08-29 14:48:04 +0300

    fix action-10

--- .github/actions/vectorize/index.js
+++ .github/actions/vectorize/index.js
@@ -24,7 +24,7 @@ try {
 } catch {
     collection = await client.createCollection({
         name: "translation-system",
-        metadata: {}
+        metadata: { source: "vectorize-action" }
     });
 }
 
